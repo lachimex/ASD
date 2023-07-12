83333c83ca6b1c27883957e6e7e4b1cde5f04e81
Michał Dydek <dydmichal@gmail.com>
2023-07-12 19:12:05 +0200

    zmiana dfs

--- dfs.py
+++ dfs.py
@@ -1,18 +1,34 @@
-graf = [[0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0]]
+# dfs
 
 
-def dfs(graf, root):
-    print("odwiedziłem wierzchołek nr ", root)
-    n = len(graf)
-    for i in range(n):
-        if graf[root][i] == 1:
-            graf[root][i] = 0
-            graf[i][root] = 0
-            dfs(graf, i)
+def dfs(G, s):
+    n = len(G)
+    visited = [False for _ in range(n)]
+    parent = [-1 for _ in range(n)]
+    d = [-1 for _ in range(n)]
+    time = 0
+
+    def dfs_visit(G, v):
+        nonlocal time, visited, parent, d
+        d[v] = time
+        time += 1
+        visited[v] = True
+        for u in G[v]:
+            if not visited[u]:
+                parent[u] = v
+                dfs_visit(G, u)
+
+    dfs_visit(G, s)
+    print(parent)
+    print(d)
 
 
-dfs(graf, 0)
+G = [
+    [2, 4],
+    [2, 3],
+    [0, 1, 4],
+    [1, 4],
+    [0, 3, 2]
+]
+
+dfs(G, 0)
